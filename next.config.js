dc89b21989da860f2f99108c98ecf2c79cb8f063
Amit Mudgal <amitmudgal@Amits-MacBook-Pro.local>
2026-05-15 09:36:04 -0700

    Add explicit redirects for legal page URLs

--- next.config.js
+++ next.config.js
@@ -37,6 +37,20 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/privacy-policy',
+        destination: '/privacy-policy/',
+        permanent: true,
+      },
+      {
+        source: '/terms-and-conditions',
+        destination: '/terms-and-conditions/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
